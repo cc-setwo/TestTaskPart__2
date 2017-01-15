10a7b21359ca1c13e7ad5cccd5a3fcd6ed6b4698
DESKTOP-R1NT098\mrscr <cc.setwo@gmail.com>
2017-01-15 14:34:56 +0200

    no message

---
+++ Assets/GoToUrl.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoToUrl : MonoBehaviour
+{
+
+    private string Url = "http://wear-studio.com/"
+
+;   private void OnMouseDown()
+    {
+        Application.OpenURL(Url);
+    }
+}
